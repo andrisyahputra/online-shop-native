e1f90099daa733a62af036276b861d8792128b3d
Andri Syahputra <65515899+andrisyahputra@users.noreply.github.com>
2023-07-29 09:57:41 +0700

    dashboard

---
+++ admin/dashboard.php
@@ -0,0 +1,5 @@
+<div class="shadow p-3 mb-3 bg-white rouded">
+    <h5>
+        <b>Selamat Dtaang Administrator</b>
+    </h5>
+</div>
---
+++ admin/produk.php
@@ -0,0 +1,5 @@
+<div class="shadow p-3 mb-3 bg-white rouded">
+    <h5>
+        <b>Halaman Produk</b>
+    </h5>
+</div>
