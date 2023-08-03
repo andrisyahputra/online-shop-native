510ffd4c18614564711bccea157dddf4074398e9
Andri Syahputra <65515899+andrisyahputra@users.noreply.github.com>
2023-08-04 00:30:24 +0700

    footer

--- index.php
+++ index.php
@@ -207,6 +207,130 @@
             </div>
         </section>
         <!-- produk section end -->
+
+        <!-- kontak mulai -->
+        <section class="kontak">
+            <div class="judul"><span>Kontak</span> Kami</div>
+            <div class="row">
+                <div class="col-md-6 kontak-map">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63709.55260437139!2d98.46000389159218!3d3.6224091779540846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3030d60114970f8d%3A0x3039d80b220cbd0!2sBinjai%2C%20Kota%20Binjai%2C%20Sumatera%20Utara!5e0!3m2!1sid!2sid!4v1691080784795!5m2!1sid!2sid"
+                        allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <div class="col-md-6 kontak-form">
+                    <form method="post">
+                        <div class="card">
+                            <div class="card-body">
+
+                                <div class="form-group row">
+                                    <label for="" class="col-sm-4 col-form-label">
+                                        Nama Lengkap :
+                                    </label>
+                                    <div class="col-sm-8">
+                                        <div class="mb-3">
+                                            <input type="text" class="form-control" name="nama"
+                                                placeholder="Masukkan Nama Lengkap Anda ." required>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="" class="col-sm-4 col-form-label">
+                                        Email :
+                                    </label>
+                                    <div class="col-sm-8">
+                                        <div class="mb-3">
+                                            <input type="email" class="form-control" name="email"
+                                                placeholder="Masukkan Email  Anda ." required>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="" class="col-sm-4 col-form-label">
+                                        No WA :
+                                    </label>
+                                    <div class="col-sm-8">
+                                        <div class="mb-3">
+                                            <input type="number" class="form-control" name="nowa"
+                                                placeholder="Masukkan Nomor WA Anda ." required>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="" class="col-sm-4 col-form-label">
+                                        Pesan :
+                                    </label>
+                                    <div class="col-sm-8">
+                                        <div class="mb-3">
+                                            <textarea type="number" class="form-control" name="nowa"
+                                                placeholder="Masukkan Nomor Pesan Anda ." required></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="text-right">
+                                    <button type="button" name="kirim" class="btn btn-primary">Kirim</button>
+                                </div>
+
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
+        <!-- kontak akhir-->
+
+
+    </div>
+    <!-- footer mulai-->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <h3>Halaman Utama</h3>
+                    <ul class="footer-menu">
+                        <li><a href="#">Beranda</a></li>
+                        <li><a href="#">Tentang Kami</a></li>
+                        <li><a href="#">Produk</a></li>
+                        <li><a href="#">Kontak</a></li>
+                    </ul>
+                </div>
+                <div class="col-4">
+                    <h3>Hubungi kami</h3>
+                    <ul class="footer-kotak">
+                        <b><i class="fas fa-store"></i> Toko Online</b>
+                        <br>
+                        <b><i class="fas fa-city"></i> Binjai</b>
+                        <br>
+                        <b><i class="fas fa-map-marker-alt"></i> Perjuangan</b>
+                        <br>
+                        <b><i class="fas fa-phone"></i> 081278391690</b>
+                        <br>
+                        <b><i class="fas fa-envelope"></i>andrisyahputra2209@gmail.com</b>
+                        <br>
+                        <b><i class="fas fa-user"></i>Andri Syahputra</b>
+                    </ul>
+                </div>
+                <div class="col-4">
+                    <h3>Social Media kami</h3>
+                    <ul class="footer-social">
+                        <a href="#"><i class="fab fa-facebook"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></i></a>
+                        <a href="#"><i class="fab fa-youtube"></i></i></a>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+    <!-- footer akhir -->
+
+    <!-- hak cipta -->
+    <div class="created">
+        <p>Created By <a href="#">Andri Syahputra</a>. | &copy; 2023</p>
     </div>
 
 
